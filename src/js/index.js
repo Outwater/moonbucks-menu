8bf43e141b6da3c35e49d7af4ed47b0456130c19
Outwater <kocon135@gmail.com>
2022-01-13 17:38:16 +0900

    refactor: 중복제거 및 함수화를 통한 코드개선

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 function App() {
-  const $ul = document.getElementById("espresso-menu-list");
-  /*
-  menu: {id:1, name:킹에스프레소}
-  */
+  const $ = (selector) => document.querySelector(selector);
+
+  this.init = () => {
+    this.renderCount();
+    this.renderList();
+  };
+
   this.state = {
-    menus: [{ id: 1, name: "킹에스프레소" }],
+    espressoMenuList: [],
+    //menu: {id:1, name:킹에스프레소}
   };
 
   this.setState = (newState) => {
@@ -16,35 +20,36 @@ function App() {
 
   //* 상태변경 Methods
   this.addMenu = (name) => {
-    const { menus } = this.state;
-    const menu = { id: Math.max(...menus.map((v) => v.id)) + 1, name };
-    this.setState({ menus: [...menus, menu] });
+    const { espressoMenuList } = this.state;
+    const menu = {
+      id: Math.max(...espressoMenuList.map((v) => v.id), 0) + 1,
+      name,
+    };
+    this.setState({ espressoMenuList: [...espressoMenuList, menu] });
   };
 
   this.editMenu = (id, name) => {
-    const { menus } = this.state;
+    const { espressoMenuList } = this.state;
     this.setState({
-      menus: menus.map((menu) => {
+      espressoMenuList: espressoMenuList.map((menu) => {
         return menu.id === id ? { id, name } : menu;
       }),
     });
   };
 
   this.removeMenu = (id) => {
-    const { menus } = this.state;
+    const { espressoMenuList } = this.state;
     this.setState({
-      menus: menus.filter((menu) => menu.id !== id),
+      espressoMenuList: espressoMenuList.filter((menu) => menu.id !== id),
     });
   };
 
   //* View
   this.renderList = () => {
-    const { menus } = this.state;
-    $ul.innerHTML = `${menus
-      .map(
-        (m) => `
-        <li class="menu-list-item d-flex items-center py-2" data-id="${m.id}">
-          <span class="w-100 pl-2 menu-name">${m.name}</span>
+    const { espressoMenuList } = this.state;
+    const menuItemTemplate = (espressoMenu) => `
+        <li class="menu-list-item d-flex items-center py-2" data-id="${espressoMenu.id}">
+          <span class="w-100 pl-2 menu-name">${espressoMenu.name}</span>
           <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -58,52 +63,57 @@ function App() {
             삭제
           </button>
         </li>
-        `
-      )
-      .join("")}`;
+    `;
+    $("#espresso-menu-list").innerHTML = `
+      ${espressoMenuList.map((menu) => menuItemTemplate(menu)).join("")}
+      `;
   };
 
   this.renderCount = () => {
-    const { menus } = this.state;
-    const $count = document.querySelector(".menu-count");
-    $count.innerHTML = `총 ${menus.length}개`;
+    const { espressoMenuList } = this.state;
+    $(".menu-count").innerHTML = `총 ${espressoMenuList.length}개`;
   };
 
   //* Event처리
-  document
-    .getElementById("espresso-menu-form")
-    .addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-  document
-    .getElementById("espresso-menu-name")
-    .addEventListener("keyup", ({ key, target }) => {
-      if (key === "Enter") {
-        if (target.value === "") return;
-        this.addMenu(target.value);
-        target.value = "";
-      }
-    });
-  document
-    .getElementById("espresso-menu-submit-button")
-    .addEventListener("click", () => {
-      const $input = document.getElementById("espresso-menu-name");
-      if ($input.value === "") return;
-      this.addMenu($input.value);
-      $input.value = "";
-    });
+  const handleAddMenu = () => {
+    const $input = $("#espresso-menu-name");
+    if ($input.value === "") return;
+    this.addMenu($input.value);
+    $input.value = "";
+  };
+  const handleEditMenu = (id) => {
+    const menuName = this.state.espressoMenuList.filter((m) => m.id === id)[0]
+      .name;
+    const editedMenuName = prompt("수정 할 메뉴이름은 무엇인가요?", menuName);
 
-  //* 수정 삭제 이벤트 (이벤트위임 by 버블링)
-  document
-    .getElementById("espresso-menu-list")
-    .addEventListener("click", ({ target }) => {
-      const id = Number(target.closest("[data-id]").dataset.id);
-      if (target.classList.contains("menu-edit-button")) {
-        const editName = prompt("수정 할 메뉴이름은 무엇인가요?");
-        this.editMenu(id, editName);
-      } else if (target.classList.contains("menu-remove-button")) {
-        confirm(`정말 ${id}번 메뉴를 삭제하시겠습니까?`) && this.removeMenu(id);
-      }
-    });
+    if (!editedMenuName) return;
+    this.editMenu(id, editedMenuName);
+  };
+  const handleRemoveMenu = (id) => {
+    confirm(`정말 ${id}번 메뉴를 삭제하시겠습니까?`) && this.removeMenu(id);
+  };
+
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  $("#espresso-menu-name").addEventListener("keyup", ({ key }) => {
+    if (key !== "Enter") return;
+    handleAddMenu();
+  });
+
+  $("#espresso-menu-submit-button").addEventListener("click", handleAddMenu);
+
+  $("#espresso-menu-list").addEventListener("click", ({ target }) => {
+    const id = Number(target.closest("[data-id]").dataset.id);
+
+    if (target.classList.contains("menu-edit-button")) {
+      handleEditMenu(id);
+    } else if (target.classList.contains("menu-remove-button")) {
+      handleRemoveMenu(id);
+    }
+  });
+
+  this.init();
 }
 new App();

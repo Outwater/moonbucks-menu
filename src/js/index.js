7c9edc71902423b8f51a362c836c36a15b09198f
Outwater <kocon135@gmail.com>
2022-01-18 16:57:22 +0900

    refactor: MenuAPI를 통한 상태처리

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,65 @@ import { $ } from "../utils/dom.js";
 const BASE_URL = "http://localhost:3000/api";
 
 function App() {
+  const MenuApi = {
+    async getAllMenuByCategory(category) {
+      console.log("here");
+      const currentMenuList = await fetch(
+        `${BASE_URL}/category/${category}/menu`
+      ).then((res) => res.json());
+      return currentMenuList;
+    },
+    async addMenu(name, category) {
+      const data = await fetch(`${BASE_URL}/category/${category}/menu`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }).then((res) => res.json());
+      console.log("추가된 menu:", data);
+    },
+    async editMenu(category, id, name) {
+      const data = await fetch(`${BASE_URL}/category/${category}/menu/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }).then((res) => res.json());
+      console.log("수정된 data: ", data);
+    },
+    async toggleSoldoutMenu(category, id) {
+      const data = await fetch(
+        `${BASE_URL}/category/${category}/menu/${id}/soldout`,
+        { method: "PUT" }
+      );
+      console.log("품절처리한 data: ", data.json());
+    },
+    async deleteMenu(category, id) {
+      const data = await fetch(`${BASE_URL}/category/${category}/menu/${id}`, {
+        method: "DELETE",
+      });
+      console.log("삭제된 data:", data);
+    },
+  };
   //* Core
-  this.init = () => {
+  this.init = async () => {
     this.setState({ category: "espresso" });
+    const currentMenuList = await MenuApi.getAllMenuByCategory(
+      this.state.category
+    );
+    this.setState({ [this.state.category]: currentMenuList });
     setEvent();
   };
 
   this.state = {
     category: "espresso",
+    espresso: [],
+    frappuccino: [],
+    blended: [],
+    teavana: [],
+    desert: [],
   };
 
   this.setState = async (newState) => {
@@ -20,28 +71,26 @@ function App() {
 
   this.render = async () => {
     // 현재 선택된 리스트를 불러와서 해당 부분 렌더링
-    const currentMenuList = await fetch(
-      `${BASE_URL}/category/${this.state.category}/menu`
-    ).then((res) => res.json());
-    console.log("render실행");
-
-    this.renderHeader(currentMenuList);
-    this.renderList(currentMenuList);
+    this.renderHeader();
+    this.renderList();
     this.mounted();
   };
 
   this.mounted = () => {};
 
   //* View
-  this.renderHeader = (currentMenuList) => {
+  this.renderHeader = () => {
     const category = this.state.category;
+    const currentMenuList = this.state[category];
     $(".heading > h2").textContent = `${
       $(`[data-category-name='${category}']`).textContent
     } 메뉴관리`;
     $(".menu-count").innerHTML = `총 ${currentMenuList.length}개`;
   };
 
-  this.renderList = (currentMenuList) => {
+  this.renderList = () => {
+    const category = this.state.category;
+    const currentMenuList = this.state[category];
     const menuItemTemplate = (currentMenu) => `
         <li class="menu-list-item d-flex items-center py-2"
           data-id="${currentMenu.id}">
@@ -76,55 +125,36 @@ function App() {
   //* Event처리
   const handleAddMenu = async (name) => {
     const category = this.state.category;
-    const data = await fetch(`${BASE_URL}/category/${category}/menu`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name }),
-    })
-      .then((res) => res.json())
-      .catch((e) => console.log(e));
-
-    console.log("추가된 data: ", data);
-    data && this.render();
+    await MenuApi.addMenu(name, category);
+    const currentMenuList = await MenuApi.getAllMenuByCategory(category);
+    this.setState({ [category]: currentMenuList });
   };
   const handleEditMenu = async (id) => {
     const category = this.state.category;
-    const menuName = "기존이름";
+    const menuName = this.state[category].find((menu) => menu.id === id).name;
     const editedMenuName = prompt("수정 할 메뉴이름은 무엇인가요?", menuName);
 
     if (!editedMenuName) return;
-    const data = await fetch(`${BASE_URL}/category/${category}/menu/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: editedMenuName }),
-    }).then((res) => res.json());
-    console.log("수정된 data: ", data);
-    data && this.render();
+    await MenuApi.editMenu(category, id, editedMenuName);
+    const currentMenuList = await MenuApi.getAllMenuByCategory(category);
+    this.setState({ [category]: currentMenuList });
   };
   const handleRemoveMenu = async (id) => {
     if (!confirm(`정말 ${id}번 메뉴를 삭제하시겠습니까?`)) return;
 
     const category = this.state.category;
-    const data = await fetch(`${BASE_URL}/category/${category}/menu/${id}`, {
-      method: "DELETE",
-    });
-    data && this.render();
+    await MenuApi.deleteMenu(category, id);
+
+    const currentMenuList = await MenuApi.getAllMenuByCategory(category);
+    this.setState({ [category]: currentMenuList });
   };
 
   const handleClickSoldout = async (id) => {
     const category = this.state.category;
-    const data = await fetch(
-      `${BASE_URL}/category/${category}/menu/${id}/soldout`,
-      {
-        method: "PUT",
-      }
-    ).then((res) => res.json());
-    console.log("품절처리한 data: ", data);
-    data && this.render();
+    await MenuApi.toggleSoldoutMenu(category, id);
+
+    const currentMenuList = await MenuApi.getAllMenuByCategory(category);
+    this.setState({ [category]: currentMenuList });
   };
 
   const handleChangeCategory = (category) => {
